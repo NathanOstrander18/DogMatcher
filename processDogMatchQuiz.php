75b0444d858e5e3d021b4ed535db728fdb23c524
Nathan Ostrander <14nostrander@gmail.com>
2018-04-26 20:20:03 -0400

    Added quiz proccessing and updated db to hold quiz answers

---
+++ processDogMatchQuiz.php
@@ -0,0 +1,15 @@
+<?php
+require("DogMatchResults.php");
+
+//grab user from session after login
+$userId = 1;
+
+//add logic for dog selection
+$dog = "Doggo";
+
+//check if user already has taken the quiz. If so, change the user quiz answers/dog and call $quiz->update()
+
+//If the user has not taken the quiz save the new quiz results
+$quiz = new DogMatchResults($userId, $dog, $_POST['1question'], $_POST['2question'], $_POST['3question'], $_POST['4question'], $_POST['5question'], $_POST['6question']);
+$quiz->save();
+?>
